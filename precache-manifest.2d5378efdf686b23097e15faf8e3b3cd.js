33e29ed57841ebb3800177fade1be99ab931fa14
llsanketll <sanket.lamsal@gmail.com>
2020-06-11 17:38:23 +0545

    Updates

---
+++ precache-manifest.2d5378efdf686b23097e15faf8e3b3cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313388e6df7d1d86262e30d5e4fc4c0a",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "641b3cfe48a53966ecad",
+    "url": "/Lights-Out/static/css/main.af008d47.chunk.css"
+  },
+  {
+    "revision": "51b48f43fe8a2b36bf2c",
+    "url": "/Lights-Out/static/js/2.bd0e400f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lights-Out/static/js/2.bd0e400f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641b3cfe48a53966ecad",
+    "url": "/Lights-Out/static/js/main.9c074b15.chunk.js"
+  },
+  {
+    "revision": "bc04917aaf642eccd76b",
+    "url": "/Lights-Out/static/js/runtime-main.b99aa999.js"
+  }
+]);
